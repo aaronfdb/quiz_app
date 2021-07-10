ff85db2333c70fcc745e0c274606f771e00dbb12
Aaron Flowerdew-Brown <aaronfdb@gmail.com>
2021-07-10 17:51:22 +1000

    JS initial file upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const quizData = [
+    {
+        question: 'Who did Courtney Act play in season 6’s Snatch Game?',
+        a: 'Julia Child',
+        b: 'Fran Drescher',
+        c: 'Kim Kardashian',
+        correct: 'b'
+    },
+    {
+        question: 'Who was first to be eliminated in season 9?',
+        a: 'Charlie Hides',
+        b: 'Bebe Zahara Benet',
+        c: 'Latrice Royale',
+        correct: 'a'
+    },
+    {
+        question: 'How many times has Shangela competed?',
+        a: '1',
+        b: '2',
+        c: '3',
+        d: '4',
+        correct: 'd'
+    },
+    {
+        question: 'What season did Michelle start as a judge?',
+        a: '2',
+        b: '3',
+        c: '4',
+        correct: 'b'
+    },
+    {
+        question: 'Who accused Alyssa Edwards of having back rolls?',
+        a: 'Detox',
+        b: 'Coco Montrese',
+        c: 'Jade Jolie',
+        correct: 'c'
+    },
+    { 
+        question: 'Who is the only queen to ever have been disqualified from RuPaul’s Drag Race?',
+        a: 'Kelly Mantle',
+        b: 'Naysha Lopez',
+        c: 'Adore Delano',
+        d: 'Willam Belli',
+        correct: 'd'
+    },
+    {
+        question: 'What song did Season 9 winner Sasha Velour lip sync to in which she revealed rose petals hidden in her gloves and wig?',
+        a: 'It’s Not Right But It’s Okay – Whitney Houston',
+        b: 'So Emotional – Whitney Houston',
+        c: 'Stronger – Britney Spears',
+        d: 'Tell It To My Heart – Taylor Dayne',
+        correct: 'b'
+    }
+    
+]
