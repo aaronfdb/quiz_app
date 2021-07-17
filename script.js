a65ed0a55b52706edcf6c07cb0f1dfb3c5c6e400
Aaron Flowerdew-Brown <aaronfdb@gmail.com>
2021-07-17 18:00:09 +1000

    JS script file update

--- script.js
+++ script.js
@@ -52,4 +52,20 @@ const quizData = [
         correct: 'b'
     }
     
-]
+];
+
+const question
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
+let currentQuestion = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+
+
+    currentQuestion++;
+}
