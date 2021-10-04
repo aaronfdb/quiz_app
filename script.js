5f0cfd62f4e67a1a58965bfc9a71ccc861898b34
Aaron Flowerdew-Brown <aaronfdb@gmail.com>
2021-10-04 16:54:19 +1100

    JS file update

--- script.js
+++ script.js
@@ -58,6 +58,8 @@ const quizData = [
     
 ];
 
+const quiz =document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById
 ("question");
 const a_text = document.getElementById('a_text');
@@ -67,10 +69,12 @@ const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers();
     const currentQuizData = quizData[currentQuiz];
 
     questionEl.innerText = currentQuizData.question;
@@ -80,15 +84,42 @@ function loadQuiz() {
     d_text.innerText = currentQuizData.d;
 }
 
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
 
 submitBtn.addEventListener("click", () => {
     currentQuiz++;
 
-    if (currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        alert("You finished");
-    }   
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+    
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>You answered correctly ${score}/${quizData.length} questions.</h2> 
+            <button onclick="location.reload()">Reload</button>`;
+        }
+    }
 });
 
 
